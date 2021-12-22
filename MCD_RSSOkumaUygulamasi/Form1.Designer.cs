2d0d1475af25e4adeb3ad88df82d844a7804de1f
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-22 20:31:49 +0300

    Add project files.

---
+++ MCD_RSSOkumaUygulamasi/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace MCD_RSSOkumaUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnKaynagi = new System.Windows.Forms.Button();
+            this.lstBaslik = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(58, 49);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(1022, 22);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = " https://www.sabah.com.tr/rss/spor.xml";
+            // 
+            // btnKaynagi
+            // 
+            this.btnKaynagi.Location = new System.Drawing.Point(1106, 31);
+            this.btnKaynagi.Name = "btnKaynagi";
+            this.btnKaynagi.Size = new System.Drawing.Size(75, 40);
+            this.btnKaynagi.TabIndex = 1;
+            this.btnKaynagi.Text = "Kaynağı";
+            this.btnKaynagi.UseVisualStyleBackColor = true;
+            this.btnKaynagi.Click += new System.EventHandler(this.btnKaynagi_Click);
+            // 
+            // lstBaslik
+            // 
+            this.lstBaslik.FormattingEnabled = true;
+            this.lstBaslik.ItemHeight = 16;
+            this.lstBaslik.Location = new System.Drawing.Point(58, 114);
+            this.lstBaslik.Name = "lstBaslik";
+            this.lstBaslik.Size = new System.Drawing.Size(188, 420);
+            this.lstBaslik.TabIndex = 2;
+            this.lstBaslik.Tag = "";
+            this.lstBaslik.SelectedIndexChanged += new System.EventHandler(this.lstBaslik_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(276, 114);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(804, 420);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Haber Başlığı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Haberin Detayı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 559);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lstBaslik);
+            this.Controls.Add(this.btnKaynagi);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnKaynagi;
+        private System.Windows.Forms.ListBox lstBaslik;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MCD_RSSOkumaUygulamasi/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MCD_RSSOkumaUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaynagi_Click(object sender, EventArgs e)
+        {
+            List<Haber> kayitlar = XMLCevir();
+            lstBaslik.DataSource = kayitlar;
+        }
+
+        private List<Haber> XMLCevir()
+        {
+            List<Haber> HaberKayitlari = new List<Haber>();
+
+            XDocument XMLKaynak = XDocument.Load(txtURL.Text);
+            List<XElement> Rows = XMLKaynak.Descendants("item").ToList();
+            foreach (XElement item in Rows)
+            {
+                Haber temp = new Haber();
+                temp.Baslik = item.Element("title").Value;
+                temp.Link = item.Element("link").Value;
+                temp.Aciklama = item.Element("description").Value;
+                HaberKayitlari.Add(temp);
+            }
+            return HaberKayitlari;
+        }
+
+        private void lstBaslik_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox SecilenDeger = (ListBox)sender;
+            Haber SecilenHaber = (Haber)SecilenDeger.SelectedItem;
+            webBrowser1.DocumentText = SecilenHaber.Aciklama;
+        }
+    }
+}
